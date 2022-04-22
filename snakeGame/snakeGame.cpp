7ba5b900b003c51f9f9012ef0df4f6d6e4c58be9
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-04-22 13:45:55 +0800

    Add files via upload

--- snakeGame/snakeGame.cpp
+++ snakeGame/snakeGame.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<conio.h>
 #include<Windows.h>
-#include <cstdlib>
+#include<cstdlib>
 
 constexpr int boxHeight = 20, boxWidth = 20;
 
@@ -12,31 +12,45 @@ struct Pos {
 
 std::vector<Pos> snake = {};
 Pos fruit;
-int8_t snakeLength = 0;
+int8_t snakeLength = 1;
 
 char lastKey = 0;
 
-bool fruitEaten = false;
+void clearScreen()
+{
+	COORD cursorPosition;	cursorPosition.X = 0;	cursorPosition.Y = 0;	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+}
+
+
 void update() {
-	system("cls");
+	clearScreen();
 	if (_kbhit()) {
 		switch (_getch()) {
+			if (lastKey != 's') {
 		case 'w':
 		case 'W':
 			lastKey = 'w';
 			break;
+			}
+			if (lastKey != 'w') {
 		case 's':
 		case 'S':
 			lastKey = 's';
 			break;
+			}
+			if (lastKey != 'd') {
 		case 'a':
 		case 'A':
 			lastKey = 'a';
 			break;
+			}
+			if (lastKey != 'a') {
 		case 'd':
 		case 'D':
 			lastKey = 'd';
 			break;
+			}
 		}
 	}
 
@@ -56,6 +70,7 @@ void update() {
 		snake[0].x += 1;
 		break;
 	}
+	snake.push_back({ snake[0].x, snake[0].y});
 
 	if (snake.size() > 1) {
 		for (int i = 2; i < snake.size(); i++) {
@@ -65,36 +80,36 @@ void update() {
 	}
 
 	if (snake[0].x == fruit.x && snake[0].y == fruit.y) {
-		fruitEaten = true;
-	}
-
-	if (fruitEaten) {
 		int fruitX = rand() % 49 + 0;
 		int fruitY = rand() % 49 + 0;
 
 		fruit = { fruitX, fruitY };
 		snakeLength++;
-		fruitEaten = false;
 	}
 }
 
 void draw() {
-	system("CLS"); 
+	clearScreen();
 	for (int j = 0; j < boxWidth; j++)
 		std::cout << "||";
 
-	for (int i = 0; i < boxHeight; i++) {
-		std::cout << "\n||";
-		for (int j = 0; j < boxWidth; j++) {
-			if (snake[0].x == j && snake[0].y == i) {
-				std::cout << "HH";
+	for (int k = 0; k < snakeLength; k++) {
+		for (int i = 0; i < boxHeight; i++) {
+			std::cout << "\n||";
+			for (int j = 0; j < boxWidth; j++) {
+				if (snake[0].x == j && snake[0].y == i) {
+					std::cout << "HH";
+				}
+				else if (fruit.x == j && fruit.y == i) {
+					std::cout << "FF";
+				}
+				else if (snake[k].x == j && snake[k].y == i) {
+					std::cout << "BB";
+				}
+				else std::cout << "  ";
 			}
-			else if (fruit.x == j && fruit.y == i) {
-				std::cout << "FF";
-			}
-			else std::cout<<"  ";
+			std::cout << "||";
 		}
-		std::cout << "||";
 	}
 
 	std::cout << "\n";
@@ -107,9 +122,17 @@ int main() {
 	int startSnakeY = rand() % boxHeight;
 	snake.push_back({ startSnakeY, startSnakeX });
 	
-	while (1) {
+	while (true) {
 		draw();
 		update();
-		Sleep(300);
+
+		if (snake[0].x == boxWidth + 1 || snake[0].x == -1 || snake[0].y == boxHeight + 1 || snake[0].y == -1) {
+			clearScreen();
+			std::cout << "You lose\n";
+			std::cout << "Your score was: " << snakeLength;
+			break;
+		}
+
+		Sleep(100);
 	}
 }
